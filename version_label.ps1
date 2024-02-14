b1ab8ce94f88e95d32f7916b7ac89dbbe2ea202d
boomhut <56619040+boomhut@users.noreply.github.com>
2024-02-14 03:26:30 +0100

    Delete version_label.ps1

--- version_label.ps1
+++
@@ -1,9 +0,0 @@
-# script to set the version label for the current build
-# usage: version_label.ps1 <version_label>
-
-param(
-    [string]$version_label
-)
-
-# set the version label to git tag formatted as: git tag -a "v1.5.0-beta" -m "version v1.5.0-beta"
-git tag -a "v$version_label" -m "version v$version_label"
